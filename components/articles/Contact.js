d72f5b335a11f38644de0c13e825d9decb617b89
mib-markus <markus.beck@vkb.de>
2020-03-27 08:49:52 +0100

    delete comments

--- components/articles/Contact.js
+++ components/articles/Contact.js
@@ -1,17 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-// import { loadCSS } from "fg-loadcss";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-// import Icon from "@material-ui/core/Icon";
 
 export default function Intro() {
-//   React.useEffect(() => {
-//     loadCSS(
-//       "https://use.fontawesome.com/releases/v5.12.0/css/all.css",
-//       document.querySelector("#font-awesome-css")
-//     );
-//   }, []);
 
   return (
     <div>
@@ -41,13 +33,11 @@ export default function Intro() {
       <ul className="icons">
         <li>
           <a href="https://de.linkedin.com/in/markus-beck?trk=people-guest_people_search-card">
-            {/* <Icon className="fab fa-linkedin" style={{ fontSize: 38 }} /> */}
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
         </li>
         <li>
           <a href="https://github.com/mib-markus">
-          {/* <Icon className="fab fa-github" style={{ fontSize: 38 }} /> */}
             <FontAwesomeIcon icon={faGithub} />
           </a>
         </li>
