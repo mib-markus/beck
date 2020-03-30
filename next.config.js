4dc20ae3abf1c3279332ccbed35f7e7a276e89cb
mib-markus <markus.beck@vkb.de>
2020-03-30 13:06:49 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,45 +1,50 @@
-const path = require('path')
-const glob = require('glob')
+const path = require("path");
+const glob = require("glob");
 const debug = process.env.NODE_ENV !== "production";
+// const withPlugins = require('next-compose-plugins');
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withOptimizedImages(
-  {
-  exportPathMap: function () {
+module.exports = withOptimizedImages({
+  exportPathMap: function() {
     return {
-      "/": { page: "/" },
+      "/": { page: "/" }
       // "/about": { page: "/about" },
-    }
+    };
   },
-    //assetPrefix: '',
-    assetPrefix: !debug ? '/beck/' : '',
+  //assetPrefix: '',
+  assetPrefix: !debug ? "/beck/" : "",
   webpack: (config, { dev }) => {
     config.module.rules.push(
       {
         test: /\.(css|scss)/,
-        loader: 'emit-file-loader',
+        loader: "emit-file-loader",
         options: {
-          name: 'dist/[path][name].[ext]'
+          name: "dist/[path][name].[ext]"
         }
       },
       {
         test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+        use: ["babel-loader", "raw-loader", "postcss-loader"]
       },
       {
         test: /\.s(a|c)ss$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader',
-          { loader: 'sass-loader',
+        use: [
+          "babel-loader",
+          "raw-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
             options: {
-              outputStyle: 'compressed', // These options are from node-sass: https://github.com/sass/node-sass
-              includePaths: ['styles', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
+              outputStyle: "compressed", // These options are from node-sass: https://github.com/sass/node-sass
+              includePaths: ["styles", "node_modules"]
+                .map(d => path.join(__dirname, d))
+                .map(g => glob.sync(g))
                 .reduce((a, c) => a.concat(c), [])
             }
           }
         ]
       }
-    )
-    return config
+    );
+    return config;
   }
-}
+});
