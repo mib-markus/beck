334abf4c8ae002edac9e4d2904173e4be75af6af
mib-markus <markus.beck@vkb.de>
2020-03-30 13:55:25 +0200

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f21ca50cfb03b489dfb1";
+/******/ 	var hotCurrentHash = "5870268a65cb19baa761";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
