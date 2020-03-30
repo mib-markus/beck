56e18b2f55a86710be8a0a1afec238fc5d80c142
mib-markus <markus.beck@vkb.de>
2020-03-30 13:05:24 +0200

    add img to next

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 const glob = require('glob')
 const debug = process.env.NODE_ENV !== "production";
 
-module.exports = {
+module.exports = withOptimizedImages(
+  {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
